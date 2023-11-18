af61ebd63a0e863ab1a66a2100d0c5e858a8baea
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2023-11-18 17:22:55 +0000

    log ous system

--- chech_pri.js
+++ chech_pri.js
@@ -1,3 +1,11 @@
 if(window.sessionStorage.pri != "yes"){
   window.location.href = "pri.html";
 }
+
+function logout(){
+  window.sessionStorage.pri = "no";
+  if(window.sessionStorage.pri != "yes"){
+    alert("Please log in")
+    window.location.href = "games.html";
+  }
+}
