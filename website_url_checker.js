a99a6001c24ab4080e7f072c9d402efb65d5b4a2
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2023-11-18 14:28:55 +0000

    coment

--- website_url_checker.js
+++ website_url_checker.js
@@ -1,4 +1,4 @@
 if(document.location.href == "https://house-777.danfos.repl.co/"){
   alert("Error,\n websit dose not exist")
-  window.location.href = "login.html";
+  window.location.href = "go_to_github_pages.html";
 }
